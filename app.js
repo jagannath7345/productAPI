ba36cccbdcb5f9d1cdfbfd118291e825a0cd668d
Jagannath <guptajn7345@gmail.com>
2023-03-13 17:12:13 +0530

    adding cors middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const connectDB = require("./db/connectDB");
 const productRoutes = require("./routes/productRoutes");
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT;
@@ -9,6 +10,8 @@ const PORT = process.env.PORT;
 app.get("/", (req, res)=>{
     res.send("hello");
 })
+app.use(cors())
+app.use(express.json())
 app.use("/api/products", productRoutes)
 const start = async()=>{
     try {
